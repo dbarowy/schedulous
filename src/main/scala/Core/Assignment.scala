fcbe408e632066004c6de17adc1c13c858e41343
dbarowy <dbarowy@users.noreply.github.com>
2016-06-06 21:05:34 -0400

    change assignment IDs to UUIDs to ensure that we don't accidentally try to use them anywhere in the solver.

--- src/main/scala/Core/Assignment.scala
+++ src/main/scala/Core/Assignment.scala
@@ -1,21 +1,16 @@
 package Core
 
+import java.util.UUID
 import smtlib.parser.Terms.SSymbol
 
 object Assignment {
-  var counter = 0
-  def nextID() = {
-    val c = counter
-    counter += 1
-    c
-  }
   def apply(slotname: SSymbol, slot: Dateslot, person: Person, approval: Approval) : Assignment = {
-    val id = Assignment.nextID()
+    val id = UUID.randomUUID()
     Assignment(id, slotname, slot, person, approval)
   }
 }
 
-case class Assignment(id: Int, slotname: SSymbol, slot: Dateslot, person: Person, approval: Approval) {
+case class Assignment(id: UUID, slotname: SSymbol, slot: Dateslot, person: Person, approval: Approval) {
   def approve() : Assignment = Assignment(id, slotname, slot, person, Approved)
   def reject() : Assignment = Assignment(id, slotname, slot, person, Rejected)
   def undecided() : Assignment = this
